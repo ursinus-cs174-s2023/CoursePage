3d266d765c509ec925fce555340602287d9f17ea
Chris Tralie <chris.tralie@gmail.com>
2022-09-13 12:45:00 -0400

    Extending HW1

--- ClassExercises/Week3_Pointers_Arrays/array.c
+++ ClassExercises/Week3_Pointers_Arrays/array.c
@@ -4,7 +4,8 @@
 #include <stdio.h>
 int main() {
     int arr[5] = {100, 200, 500, 1000, 2000};
-    printf("%u\n", arr);
-    printf("%u\n", &arr[4]);
+    printf("%i\n", &arr[4] - arr);
+    printf("arr = %u\n", arr);
+    printf("&arr[4] = %u\n", &arr[4]);
     return 0;
 }
