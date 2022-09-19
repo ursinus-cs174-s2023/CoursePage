253ec7990f5e16f92f5503daab74d11527cb1e79
Chris Tralie <chris.tralie@gmail.com>
2022-09-18 23:33:40 -0400

    Link

--- ClassExercises/Week3_Pointers_Arrays/char.c
+++ ClassExercises/Week3_Pointers_Arrays/char.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+
+void printString(char* s) {
+    while (*s != '\0') {
+        printf("%c", *s);
+        s++;
+    }
+}
+
 int main() {
-    char x[12] = {'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\0'};
+    
+    char x[12] = {'H', 'e', 'l', 'l', 'o', '\0', 'w', 'o', 'r', 'l', 'd', '\0'};
     printf("%s\n", x);
-    printf("  x    = %u\n", x);
-    printf(" x+11  = %u\n", x+11);
-    printf("&x[11] = %u\n", &x[11]);
+    //printString(x);
+    //printf("  x    = %u\n", x);
+    /*printf(" x+11  = %u\n", x+11);
+    printf("&x[11] = %u\n", &x[11]);*/
     return 0;
 }
