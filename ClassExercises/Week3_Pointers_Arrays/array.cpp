4aee88c9f356b8fa2899eda94521f4d4e83d9074
Chris Tralie <chris.tralie@gmail.com>
2023-01-30 20:36:30 -0500

    Menus

--- ClassExercises/Week3_Pointers_Arrays/array.cpp
+++ ClassExercises/Week3_Pointers_Arrays/array.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main() {
-    int arr[5] = {100, 200, 500, 1000, 2000};
+    int favNums[5] = {100, 200, 500, 1000, 2000};
+    int* arr = favNums;
     cout << &arr[4] - arr << "\n";
     cout << "arr     = " << (uint64_t)arr << "\n";
     cout << "&arr[4] = " << (uint64_t)&arr[4] << "\n";
--- ClassExercises/Week3_Pointers_Arrays/array2.cpp
+++ ClassExercises/Week3_Pointers_Arrays/array2.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 int main() {
-    int arr[5] = {100, 200, 500, 1000, 2000};
-    int x = *(arr+4);
+    long arr[5] = {100, 200, 500, 1000, 2000};
+    int length = 5;
+    long x = *(arr+1000000); // "Pointer arithmetic"
+    // When we add a number to a polonger, it actually
+    // moves us by sizeof(long)
+    // x + k, is equivalent to the address
+    // x + k*sizeof(long)
+    cout << "sizeof(long) " << sizeof(long) << "\n";
     cout << x << "\n";
     return 0;
 }
